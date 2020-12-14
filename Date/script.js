b4f1b84e1e2cb3bc949eccfce81cada9e147eb5d
trunghieu <trunghieu.ngo612@gmail.com>
2020-12-14 23:49:11 +0700

    Use Date

---
+++ Date/script.js
@@ -0,0 +1,12 @@
+var d = new Date();
+
+document.write(d.getDate()      + "<br/>");
+document.write(d.getDay()       + "<br/>");
+document.write(d.getFullYear()  + "<br/>");
+document.write(d.getYear()      + "<br/>");
+document.write(d.getHours()     + "<br/>");
+document.write(d.getMilliseconds() + "<br/>");
+document.write(d.getMinutes()   + "<br/>");
+document.write(d.getMonth()     + "<br/>");
+document.write(d.getSeconds()   + "<br/>");
+document.write(d.getTime()      + "<br/>");
